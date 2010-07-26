bca3d9586c04581bd9cc5d705dfee07c73f5392a
rx <rx@voomify.com>
2010-07-26 14:54:07 -0400

    update db_migrate.sh for a clean install

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ -s '/usr/local/lib/rvm' ]] && source '/usr/local/lib/rvm'
+rake production db:create
 rake production db:migrate
 rake production db:migrate:extensions
 rake production radiant:extensions:reorder:update
