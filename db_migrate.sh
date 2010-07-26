0cb78c56a0cf64fc8be9425a20488dd0c47adca6
rx <rx@voomify.com>
2010-07-26 14:24:36 -0400

    rolled back to stock import export

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -s '/usr/local/lib/rvm' ]] && source '/usr/local/lib/rvm'
-rake radiant:extensions:reorder:migrate
+rake radiant:extensions:migrate
 rake radiant:extensions:reorder:update
