c4b71d296314b36962c4961dd76a511f6fb7fed7
rx <rx@voomify.com>
2010-08-02 16:02:32 -0400

    db_migrate change

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 [[ -s '/usr/local/lib/rvm' ]] && source '/usr/local/lib/rvm'
-rvm use ree-1.8.7
 rake production db:create
 rake production db:migrate
 rake production db:migrate:extensions
