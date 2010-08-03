0149cf733e427e1adeecf0f9eecac15c1c0b570f
rx <rx@voomify.com>
2010-08-03 14:32:24 -0400

    removed rvm infor from dbmigrate

---
+++ config/initializers/paperclipped.rb
@@ -0,0 +1,11 @@
+#Radiant::Config["assets.additional_thumbnails"] #default: 'normal=normal=640x640>'
+#Radiant::Config["assets.storage"]="s3"
+#Radiant::Config["assets.s3.key"]
+#Radiant::Config["assets.s3.secret"]
+#Radiant::Config["assets.s3.bucket"]
+#Radiant::Config["assets.s3.url"]
+#Radiant::Config["assets.s3.path"]
+#Radiant::Config["assets.content_types"] #default: 'image/jpeg, image/pjpeg, image/gif, image/png, image/x-png, image/jpg, video/x-m4v, video/quicktime, application/x-shockwave-flash, audio/mpeg, video/mpeg;
+#Radiant::Config["assets.display_size"] #default: 'original'
+#Radiant::Config["assets.skip_filetype_validation"] #default: true
+#Radiant::Config["assets.max_asset_size"] #default: 5
