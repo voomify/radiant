971b1ceeb7e2596bfd954dd430b357bb8f8406fc
rx <rx@voomify.com>
2010-08-03 14:32:24 -0400

    removed rvm infor from dbmigrate

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-[[ -s '/usr/local/lib/rvm' ]] && source '/usr/local/lib/rvm'
 rake production db:create
 rake production db:migrate
 rake production db:migrate:extensions
 rake production radiant:extensions:reorder:update
+rake production radiant:extensions:paperclipped:update
