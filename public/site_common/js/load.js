0dc5b39bafb0338a03cccbbc27d78989a0946a29
Kurt Grafius <kurtg@kurtg-mint-dev.(none)>
2010-08-05 10:30:17 -0700

    added public images

---
+++ public/site_common/js/load.js
@@ -0,0 +1,23 @@
+function require(jspath) {
+    document.write('<script type="text/javascript" src="'+jspath+'"><\/script>');
+}
+
+// jQuery Nivo Slider
+require("assets/js/jquery.nivo.slider.pack.js");
+
+// jQuery Social Tooltip
+require("assets/js/jquery.tipsy.js");
+
+// jQuery Super Fish Navigation
+require("assets/js/hoverIntent.js");
+require("assets/js/superfish.js");
+
+// jQuery FancyBox
+require("assets/js/jquery.prettyPhoto.js");
+
+// Theme Core File
+require("assets/js/aspire.js");
+
+// Cufon Font Replacement
+require("assets/js/cufon-yui.js");
+require("assets/js/avenir.js");
