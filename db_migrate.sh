781062a5cc36b9e439a52d01831b0a52ff7b64a4
rx <rx@voomify.com>
2010-08-31 10:50:00 -0400

    updated highrise_contact extension

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,2 +1,3 @@
 rake production db:migrate db:migrate:extensions
-rake production radiant:extensions:reorder:update radiant:extensions:paperclipped:update radiant:extensions:custom_fields:update
+rake production radiant:extensions:reorder:update radiant:extensions:paperclipped:update radiant:extensions:custom_fields:update radiant:extensions:highrise_contact:update
+
