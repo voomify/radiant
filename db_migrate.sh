cd2d498630c3afa44073303295964b9e257d63fe
russell <russell@voomify.com>
2011-07-18 09:56:32 -0400

    redeploy

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,4 +1,4 @@
 # uncomment these when you are pushing to a new server ... or have installed new extensions
-# rake production db:migrate db:migrate:extensions
-# rake production radiant:extensions:reorder:update radiant:extensions:paperclipped:update radiant:extensions:custom_fields:update radiant:extensions:highrise_contact:update
+rake production db:migrate db:migrate:extensions
+rake production radiant:extensions:reorder:update radiant:extensions:paperclipped:update radiant:extensions:custom_fields:update radiant:extensions:highrise_contact:update
 
